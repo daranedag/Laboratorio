37310cf57ad6e557f96ba208204abfd2a4c6d934
daranedag <daranedag@gmail.com>
2014-09-23 12:58:42 -0300

    Laboratorio0.8
    Imagen insertada =)
    Nombres insertados
    Agregar tabla y diseño para datos de informe de suelos
    Pedir exportar datos del pc para leerlos

--- AppSuelos/src/appsuelos/AppSuelos.java
+++ AppSuelos/src/appsuelos/AppSuelos.java
@@ -23,6 +23,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.util.IOUtils;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 /**
  *
  * @author DieGui
@@ -48,6 +49,7 @@ public class AppSuelos {
             v1 = new Inicio();
             v1.setVisible(true);
             generarInformeSuelos();
+            
             //leerArchivo("D:\\Dropbox\\readme.txt");            
         }      
         catch(Exception e) {
@@ -72,6 +74,11 @@ public class AppSuelos {
     public static void generarInformeSuelos() throws IOException{
         Workbook libro = new HSSFWorkbook();  //crear libro
         Sheet hoja = libro.createSheet("HojaDePrueba");
+        PrintSetup ps = hoja.getPrintSetup();
+        hoja.setAutobreaks(true);
+        ps.setFitHeight((short) 1);
+        ps.setFitWidth((short) 1);
+        agregarImagenInforme(libro, hoja);
         String file = "pruba.xls";
         try{
             FileOutputStream out = new FileOutputStream(file);
@@ -120,24 +127,27 @@ public class AppSuelos {
         }
     }
     
-    /*
+    
     public static void agregarImagenInforme(Workbook wb, Sheet h) throws FileNotFoundException, IOException{
         try{
-            //add picture data to this workbook.
-            InputStream is = new FileInputStream("C:\\Users\\DieGui\\Documents\\GitHub\\Laboratorio\\AppSuelos\\src\\appsuelos\\logo.png");
+            InputStream is = new FileInputStream("C:\\Users\\DieGui\\Documents\\GitHub\\Laboratorio\\AppSuelos\\src\\appsuelos\\logo2.jpg");
             byte[] bytes = IOUtils.toByteArray(is);
-            int pictureIdx = wb.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
+            int pictureIdx = wb.addPicture(bytes, Workbook.PICTURE_TYPE_JPEG);
             is.close();
+
             CreationHelper helper = wb.getCreationHelper();
+
             // Create the drawing patriarch.  This is the top level container for all shapes. 
             Drawing drawing = h.createDrawingPatriarch();
+
             //add a picture shape
             ClientAnchor anchor = helper.createClientAnchor();
             //set top-left corner of the picture,
             //subsequent call of Picture#resize() will operate relative to it
-            anchor.setCol1(3);
-            anchor.setRow1(2);
+            anchor.setCol1(13);
+            anchor.setRow1(9);
             Picture pict = drawing.createPicture(anchor, pictureIdx);
+
             //auto-size picture relative to its top-left corner
             pict.resize();            
         }
@@ -145,7 +155,6 @@ public class AppSuelos {
             System.out.println(e.getMessage());
         }        
     }
-    */
     
     public static void escribirEncabezadoInforme(Workbook wb, Sheet h) throws FileNotFoundException, IOException{            
         //Estilo Celda del titulo  UNIVERSIDAD AUSTRAL DE CHILE
@@ -157,7 +166,7 @@ public class AppSuelos {
         f.setColor((short)0x7fff);
         f.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
         cs1.setFont(f);                
-        Row fila = h.createRow(1);
+        Row fila = h.createRow(2);
         Cell celda = fila.createCell(7);
         celda.setCellStyle(cs1);
         celda.setCellValue("U   N  I  V  E  R  S  I  D  A  D       A  U  S  T  R  A  L      D  E         C  H  I  L  E");
@@ -171,13 +180,13 @@ public class AppSuelos {
         f2.setColor((short)0x7fff);
         f2.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
         cs2.setFont(f2);                
-        fila = h.createRow(2);
+        fila = h.createRow(3);
         celda = fila.createCell(7);
         celda.setCellStyle(cs2);
         celda.setCellValue("LABORATORIO DE NUTRICIÓN Y SUELOS FORESTALES - FACULTAD DE CIENCIAS FORESTALES");
 
         //Estilo Celda del titulo   CASILLA 567 - VALDIVIA FONOFAX (63) 221431  E-mail  labnsf@uach.cl
-        fila = h.createRow(3);
+        fila = h.createRow(4);
         celda = fila.createCell(7);
         celda.setCellStyle(cs2);
         celda.setCellValue("CASILLA 567 - VALDIVIA FONOFAX (63) 221431  E-mail  labnsf@uach.cl");
@@ -190,7 +199,7 @@ public class AppSuelos {
         hlink_font.setUnderline(Font.U_SINGLE);
         hlink_font.setColor(IndexedColors.BLUE.getIndex());
         hlink_style.setFont(hlink_font);
-        fila = h.createRow(4);
+        fila = h.createRow(5);
         celda = fila.createCell(7);
         celda.setCellStyle(hlink_style);
         celda.setCellValue("http://www.uach.cl/labsuelosforestales");            
@@ -203,20 +212,184 @@ public class AppSuelos {
         f3.setFontHeightInPoints((short) 11);
         f3.setColor((short)0x7fff);
         f3.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-        cs3.setFont(f3);                
-        fila = h.createRow(14);
+        cs3.setFont(f3);
+        
+        CellStyle cs4 = wb.createCellStyle();
+        cs4.setAlignment(CellStyle.ALIGN_LEFT);
+        Font f4 = wb.createFont();
+        f4.setFontName("Arial");
+        f4.setFontHeightInPoints((short) 11);
+        f4.setColor((short)0x7fff);
+        cs4.setFont(f4);        
+        
+        fila = h.createRow(13);
         celda = fila.createCell(1);
         celda.setCellStyle(cs3);
-        h.setColumnWidth(1, 22*256);
+        //h.setColumnWidth(1, 22*256);
         celda.setCellValue("JEFE LABORATORIO:   ");
         celda = fila.createCell(3);
         celda.setCellStyle(cs3);
+        celda.setCellValue("GISELA ROMENY, Bioquímico");
+        fila = h.createRow(15);
+        celda = fila.createCell(1);
+        celda.setCellStyle(cs4);
+        celda.setCellValue("ANALISTAS:          ");
+        celda = fila.createCell(3);
+        celda.setCellStyle(cs4);
+        celda.setCellValue("ILONA SLEBE, Agrónomo");
+        fila = h.createRow(16);
+        celda = fila.createCell(3);
+        celda.setCellStyle(cs4);
         celda.setCellValue("GISELA ROMENY, Bioquímico");
         /*
         JEFE LABORATORIO:  GISELA ROMENY, Bioquímico
         ANALISTAS:  ILONA SLEBE, Agrónomo
                     GISELA ROMENY, Bioquímico        
         */
+        
+        //Configuración Ancho de Columnas
+        for(int i=0; i<17; i++){
+            switch(i){
+                case 0:  //A
+                    h.setColumnWidth(0, 8*256);
+                    break;
+                case 1: //B
+                    h.setColumnWidth(1, 22*256);
+                    break;
+                case 2: //C
+                    h.setColumnWidth(2, 14*256);
+                    break;
+                case 3: //D
+                    h.setColumnWidth(3, 15*256);
+                    break;
+                case 4: //E
+                    h.setColumnWidth(4, 10*256);
+                    break;
+                case 5: //F
+                    h.setColumnWidth(5, 19*256);
+                    break;    
+                case 6: //G
+                    h.setColumnWidth(6, 14*256);
+                    break;
+                case 7: //H
+                    h.setColumnWidth(7, 13*256);
+                    break;    
+                case 8: //I
+                    h.setColumnWidth(8, 10*256);
+                    break;
+                case 9://J
+                    h.setColumnWidth(9, 10*256);
+                    break;
+                case 10://K
+                    h.setColumnWidth(10, 12*256);
+                    break;
+                case 11://L
+                    h.setColumnWidth(11, 10*256);
+                    break;    
+                case 12://M
+                    h.setColumnWidth(12, 12*256);
+                    break;
+                case 13://N
+                    h.setColumnWidth(13, 10*256);
+                    break;    
+                case 14://O
+                    h.setColumnWidth(14, 10*256);
+                    break;
+                case 15://P
+                    h.setColumnWidth(15, 12*256);
+                    break;    
+                case 16://Q
+                    h.setColumnWidth(16, 3*256);
+                    break; 
+            }
+        }
+        
+        //Agregar nombres y datos de la muestra y cliente
+        fila = h.createRow(23);
+        celda = fila.createCell(1);
+        celda.setCellStyle(cs4);
+        celda.setCellValue("SOLICITANTE");
+        fila = h.createRow(24);
+        celda = fila.createCell(1);
+        celda.setCellStyle(cs4);
+        celda.setCellValue("FECHA RECEPCIÓN");
+        fila = h.createRow(25);
+        celda = fila.createCell(1);
+        celda.setCellStyle(cs4);
+        celda.setCellValue("FECHA ENTREGA");
+        fila = h.createRow(26);
+        celda = fila.createCell(1);
+        celda.setCellStyle(cs4);
+        celda.setCellValue("ARCHIVO");
+        
+        //Agregar bordes a celdas 
+        
+        //Agregar titulo del informe
+        CellStyle cs5 = wb.createCellStyle();
+        cs5.setAlignment(CellStyle.ALIGN_LEFT);
+        Font f5 = wb.createFont();
+        f5.setFontName("Arial");        
+        f5.setFontHeightInPoints((short) 12);
+        f5.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        f5.setColor((short)0x7fff);
+        cs5.setFont(f5);
+        fila = h.createRow(27);
+        celda = fila.createCell(6);  
+        celda.setCellStyle(cs5);
+        celda.setCellValue("INFORME ANALISIS QUIMICO AGUA  ");
+        
+        // Bordes de datos y estilo de celdas
+        CellStyle cs6 = wb.createCellStyle();
+        cs6.setAlignment(CellStyle.ALIGN_CENTER);
+        Font f6 = wb.createFont();
+        f6.setFontName("Arial");        
+        f6.setFontHeightInPoints((short) 12);
+        cs6.setFont(f6);
+        //Estilo borde izquierdo
+        cs6.setBorderLeft(CellStyle.BORDER_MEDIUM);
+        cs6.setLeftBorderColor(IndexedColors.BLACK.getIndex());        
+        //Estilo borde derecho
+        cs6.setBorderRight(CellStyle.BORDER_MEDIUM);
+        cs6.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        cs6.setBorderTop(CellStyle.BORDER_MEDIUM);
+        cs6.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        
+        CellStyle cs7 = wb.createCellStyle();
+        cs7.setAlignment(CellStyle.ALIGN_CENTER);
+        cs7.setFont(f6);
+        cs7.setBorderLeft(CellStyle.BORDER_MEDIUM);
+        cs7.setLeftBorderColor(IndexedColors.BLACK.getIndex());        
+        //Estilo borde derecho
+        cs7.setBorderRight(CellStyle.BORDER_MEDIUM);
+        cs7.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        cs7.setBorderTop(CellStyle.BORDER_THIN);
+        cs7.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        
+        fila = h.createRow(33);
+        for(int i=0; i<13; i++){
+            celda = fila.createCell(i);
+            celda.setCellStyle(cs6);
+            if(i==0){
+                celda.setCellValue("Nº");
+            }
+        }
+        fila = h.createRow(34);
+        for(int i=0; i<13; i++){
+            celda = fila.createCell(i);
+            celda.setCellStyle(cs7);
+            switch(i){
+                case 0:
+                    celda.setCellValue("LAB.");
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    celda.setCellValue("IDENTIFICACION");
+                    break;
+            }
+        }
+      
+
         
     }
 }
